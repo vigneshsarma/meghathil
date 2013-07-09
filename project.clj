341db32106dffc87abb2b5604ed0d0694c710bed
Vignesh Sarma K <vignesh.sarma@gmail.com>
2013-07-09 22:50:27 +0530

    google orianted rewrite

--- project.clj
+++ project.clj
@@ -6,7 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [me.raynes/fs "1.4.4"]
                  [org.clojure/tools.logging "0.2.6"]
-                 [com.google.oauth-client/google-oauth-client "1.15.0-rc"]
-                 [com.google.http-client/google-http-client-jackson "1.15.0-rc"]
-                 [com.google.apis/google-api-services-drive "v2-rev79-1.15.0-rc"]]
+                 [stuarth/clj-oauth2 "0.3.2"]
+                 [cheshire "5.2.0"]
+                 [com.google.oauth-client/google-oauth-client "1.15.0-rc"]]
   :main meghathil.core)
--- src/meghathil/core.clj
+++ src/meghathil/core.clj
@@ -1,30 +1,50 @@
 (ns meghathil.core
   (:require [me.raynes.fs :as fs]
-            [clojure.tools.logging :as log]
-            [clojure.edn :as edn])
+            ;; [clojure.tools.logging :as log]
+            [clojure.edn :as edn]
+            [cheshire.core :refer [parse-string]]
+            [clj-oauth2.client :as oauth2])
   (:gen-class))
-(import '(com.google.api.client.googleapis.auth.oauth2
-          GoogleAuthorizationCodeFlow GoogleCredential
-          GoogleTokenResponse GoogleAuthorizationCodeFlow$Builder)
-        '(com.google.api.client.http FileContent HttpTransport)
-        '(com.google.api.client.http.javanet NetHttpTransport)
-        '(com.google.api.client.json JsonFactory)
-        '(com.google.api.client.json.jackson JacksonFactory)
-        '(com.google.api.services.drive Drive DriveScopes Drive$Builder)
-        '(com.google.api.services.drive.model File))
-
 (require 'clojure.java.io)
 
-(def CLIENT_ID "355829055312-gqarc8eohg441ls0e757esgd6r5vmh3p.apps.googleusercontent.com")
-(def CLIENT_SECRET "2mqNDFHz-EfSCQB4mm1X6SDZ")
+(def google-com-oauth2
+  (edn/read-string (slurp
+                    (clojure.java.io/resource "google-com-oauth2.edn"))))
 
-(def REDIRECT_URI "urn:ietf:wg:oauth:2.0:oob")
+(def auth-req
+  (oauth2/make-auth-request google-com-oauth2))
+
+(def url-google-file "https://www.googleapis.com/drive/v2/files/")
+(def url-google-upload-file "https://www.googleapis.com/upload/drive/v2/files?uploadType=media")
 
 (defn get-auth-code [url]
   (println "Please open the following URL in your browser "
            "then type the authorization code:")
-  (println (str "  " url))
+  (println url)
   (read-line))
+
+(defn get-google-credential []
+  (let [url (:uri auth-req)
+        code (get-auth-code url)]
+    (oauth2/get-access-token google-com-oauth2
+                             {:code code} auth-req)))
+
+(defn google-upload-file [credential file]
+  (oauth2/post url-google-upload-file
+               {:oauth2 credential
+                :headers {"Content-Type" "text/plain"}
+                :throw-exceptions false
+                :body file}))
+(defn google-insert-file-with-metadata [credential file metadata]
+  (let [upload_file (google-upload-file credential file)]
+    (if (= (:status upload_file) "200")
+      (let [response (parse-string (:body upload_file))
+            fileId ("fileId" response)]
+        (oauth2/put
+         (str url-google-file fileId)
+         {:oauth2 credential
+          :throw-exceptions false
+          :file-params metadata})))))
 
 (defn insert-file []
   (let [body (new File)]
@@ -34,35 +54,23 @@
       (.setMimeType "text/plain"))
     body))
 
-(defn do-the-stuff
-  []
-  (let [httpTransport (new NetHttpTransport)
-        jsonFactory (new JacksonFactory)
-        flow (-> (new GoogleAuthorizationCodeFlow$Builder httpTransport
-                      jsonFactory CLIENT_ID CLIENT_SECRET
-                      (list (. DriveScopes DRIVE)))
-                 (.setAccessType "online")
-                 (.setApprovalPrompt "auto")
-                 (.build))
-        url (-> (.newAuthorizationUrl flow)
-                (.setRedirectUri REDIRECT_URI)
-                (.build))
-        code (get-auth-code url)
-        response (-> (.newTokenRequest flow code)
-                     (.setRedirectUri REDIRECT_URI)
-                     (.execute))
-        credential (-> (new GoogleCredential)
-                       (.setFromTokenResponse response))
+(defn do-the-stuff []
+  (let [
+        credential (get-google-credential)
         ;; Create a new authorized API client
-        service (-> (new Drive$Builder httpTransport
-                         jsonFactory credential) (.build))
-        body (insert-file)
-        fileContent (clojure.java.io/file "README.md")
-        mediaContent (new FileContent "text/plain" fileContent)
-        file (-> (.files service)
-                 (.insert body mediaContent)
-                 (.execute))]
-    (println "File ID:" (.getId file))))
+        list_files (oauth2/get
+                    url-google-file
+                    {:oauth2 credential})
+        upload_file (oauth2/post
+                     url-google-file
+                     {:oauth2 credential
+                      :mutlipart [{:name "Readme"
+                                   :content (clojure.java.io/file "README.md")}]
+                      :form-params {:title "Readme"
+                                    :mime-type "text/plain"
+                                    :description "A test document"}})]
+    ;; (println "File ID:" (.getId file))
+    (println (:body list_files) (:body upload_file))))
 
 (defn get-conf
   "simple read all the configuration, will decide what to do with in some other place"
@@ -73,8 +81,11 @@
   "I don't do a whole lot."
   [& args]
   (alter-var-root #'*read-eval* (constantly false))
-  (let [conf (get-conf "conf.edn")]
-    (log/info "Hello, World!")
-    (log/info "Conf" conf)
-    (fs/walk #(log/info %1 %2 %3) (:dir conf)))
-  (do-the-stuff))
+  ;; (let [conf (get-conf "conf.edn")]
+    ;; (log/info "Hello, World!")
+    ;; (log/info "Conf" conf)
+    ;; (fs/walk #(log/info %1 %2 %3) (:dir conf)))
+  (do-the-stuff)
+  )
+
+(def credential {:access-token "ya29.AHES6ZTiboGbzeJbgTSBOZqs9udgildd_GcsCnpit2j-pvXy0FVhXg", :token-type "Bearer", :query-param :access_token, :params {:expires_in 3600, :refresh_token "1/B1eZmo4NjtULiRHyKFV12HQPTuYj0aPQEKGJ9keqOj0"}})
