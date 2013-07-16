86f29c7dbe50f86c4d324bdda3e1662934deeb04
Vignesh Sarma K <vignesh.sarma@gmail.com>
2013-07-17 00:10:02 +0530

    com.novemberain/pantomime part of the dependency.

--- project.clj
+++ project.clj
@@ -6,6 +6,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [me.raynes/fs "1.4.4"]
                  [org.clojure/tools.logging "0.2.6"]
+                 [com.novemberain/pantomime "2.0.0"]
                  [stuarth/clj-oauth2 "0.3.2"]
                  [cheshire "5.2.0"]
                  [com.google.oauth-client/google-oauth-client "1.15.0-rc"]]
